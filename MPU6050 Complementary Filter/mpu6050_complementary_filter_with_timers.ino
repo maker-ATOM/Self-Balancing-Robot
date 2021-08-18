5b4ee13bc3e6106b661c5eab799ea5acaa0bb23f
ATOM <87944335+maker-ATOM@users.noreply.github.com>
2021-08-18 12:27:29 +0530

    Add files via upload

---
+++ MPU6050 Complementary Filter/mpu6050_complementary_filter_with_timers.ino
@@ -0,0 +1,70 @@
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+MPU6050 mpu;
+
+int accY, accZ, gyroX;
+float accAngle = 0, gyroAngle = 0, botAngle = 0, previousAngle = 0;
+float gyroRate = 0;
+
+
+
+void setup() {
+
+  mpu.initialize();
+  Serial.begin(9600);
+
+  initTimer2();
+
+}
+
+void loop() {
+
+  accZ = mpu.getAccelerationZ();
+  accY = mpu.getAccelerationY();
+
+  gyroX = mpu.getRotationX();
+
+  gyroRate = gyroX / 131;
+
+  accAngle = atan2(accZ, -accY) * RAD_TO_DEG;
+
+  botAngle = 0.97 * (previousAngle + gyroAngle) + 0.03 * (accAngle);
+  previousAngle = botAngle;
+
+  Serial.println(botAngle);
+
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+  gyroAngle = (float)gyroRate * 0.001;
+}
+
+void initTimer2()
+{
+  //reset timer2 control register A
+  TCCR2A = 0;
+
+  //set CTC mode
+  TCCR2A |= (1 << WGM21);
+  TCCR2A &= ~(1 << WGM20);
+  TCCR2B &= ~(1 << WGM22);
+
+  //prescaler of 1024
+  TCCR2B &= ~(1 << CS21);
+  TCCR2B |= ((1 << CS22) | (1 << CS20));
+
+  //reset counter
+  TCNT2 = 0;
+
+  //set compare register
+  OCR2A = 125;
+
+  //enable timer1 compare match interrupt
+  TIMSK2 |= (1 << OCIE2A);
+
+  //enable global interrupt
+  sei();
+}
